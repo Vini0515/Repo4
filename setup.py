54ca98590671f4aa774bb4e9dee7a6d76e417e85
´Vini¨ <virginiajrp0505@gmail.com>
2025-03-05 21:57:19 -0300

    setup and init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Repo4",                                   # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Vini",                         # Tu nombre
+    author_email="virginiajrp0505@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/Vini0515/Repo4",     # URL del proyecto
+)
